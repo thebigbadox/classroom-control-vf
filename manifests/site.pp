2238f45ba99c2bbe3627ef802690b89af13d6b53
thebigbadox <adamwebb13@gmail.com>
2016-09-27 13:14:05 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-  file{ '/etc/mod':
+  file{ '/etc/motd':
     ensure => file,
     owner => 'root',
     group => 'root',
