0e6a7177ca6e90349e6f21c5c8b0cc9d4620f040
thebigbadox <adamwebb13@gmail.com>
2016-09-28 10:09:47 -0500

    14.1

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -1,16 +1,20 @@
 class aliases (
     $admin = 'root',
-) {
-    # uses $admin to build the aliases file
-    file { '/etc/aliases':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => template('aliases/aliases.erb'),
-    }
-    exec { '/usr/bin/newaliases':
-        refreshonly => true,
-        subscribe   => File['/etc/aliases'],
-    }
+    ) {
+        # uses $admin to build the aliases file
+        file { '/etc/aliases':
+            ensure => file,
+            owner => 'root',
+            group => 'root',
+            mode => '0644',
+            content => epp('aliases/aliases.epp',
+            {
+            admin => $admin,
+        }),
+        }
+        exec { '/usr/bin/newaliases':
+            refreshonly => true,
+            subscribe => File['/etc/aliases'],
+        }
 }
+
