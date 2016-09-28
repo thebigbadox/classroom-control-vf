5539d0d2c2525ad94178ab143c6a57914be9bb04
thebigbadox <adamwebb13@gmail.com>
2016-09-28 11:06:50 -0500

    15.1

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,15 @@
+class{
+  define users::managed_user (
+  $group = $title,
+  ) {
+    user { $title:
+      ensure => present,
+    }
+    file { "/home/${title}":
+      ensure => directory,
+      owner => $title,
+      group => $group,
+    }
+  }
+
+}
