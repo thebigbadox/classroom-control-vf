69669335643829d1880c430e5c168c436e928946
thebigbadox <adamwebb13@gmail.com>
2016-09-28 13:05:54 -0500

    Code Sync

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,38 @@
 class nginx {
-File {
-  owner => 'root',
-  group => 'root',
-  mode => '0664',
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
   }
+
   package { 'nginx':
     ensure => present,
   }
-  file { [ '/var/www', '/etc/nginx/conf.d' ]:
-    ensure => directory,
+
+  file { '/var/www':
+    ensure  => directory,
+    require => Package['nginx'],
   }
+
   file { '/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
+
   file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
-    notify => Service['nginx'],
   }
+
   file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-    notify => Service['nginx'],
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
+
   service { 'nginx':
-    ensure => running,
-    enable => true,
+    ensure    => running,
+    subscribe => File['/etc/nginx/conf.d/default.conf', '/etc/nginx/nginx.conf'],
   }
 }
