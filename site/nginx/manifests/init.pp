375eb7a75f68c3d16744f1ed10cbda822e9fbf2f
thebigbadox <adamwebb13@gmail.com>
2016-09-28 13:40:55 -0500

    15.2

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,32 @@
 class nginx {
+  case $::osfamily {
+  'redhat','debian' : {
+    $package = 'nginx'
+    $owner = 'root'
+    $group = 'root'
+    $docroot = '/var/www'
+    $confdir = '/etc/nginx'
+    $logdir = '/var/log/nginx'
+  }
+  'windows' : {
+    $package = 'nginx-service'
+    $owner = 'Administrator'
+    $group = 'Administrators'
+    $docroot = 'C:/ProgramData/nginx/html'
+    $confdir = 'C:/ProgramData/nginx'
+    $logdir = 'C:/ProgramData/nginx/logs'
+  }
+  default : {
+    fail("Module ${module_name} is not supported on ${::osfamily}")
+  }
+  }
+  # user the service will run as. Used in the nginx.conf.epp template
+  $user = $::osfamily ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
+
   File {
     owner => 'root',
     group => 'root',
