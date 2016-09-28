0f76036e4c22c57578e9380f6061123a282ccee9
thebigbadox <adamwebb13@gmail.com>
2016-09-28 09:14:14 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -55,7 +55,7 @@ node default {
     #}
     if $::virtual != 'physical' {
       $vmname = capitalize($::virtual)
-      notify{"This is a ${vmname} virtual machine."}
+      notify { "This is a ${vmname} virtual machine.": }
     }
     
     include users
